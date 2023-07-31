ccb2a60d888fbbbc047d2dd6652063c654fc1c23
Job van der Zweep <jobvan-d@student.codam.nl>
2023-07-31 18:04:09 +0200

    seems to be working somewhat!

--- cheatsheet.md
+++ cheatsheet.md
@@ -1,2 +1,4 @@
 `docker-compose build && docker run srcs_wordpress`
 `docker run -it srcs_wordpress bash`
+
+`docker run -it --rm -v srcs_www:/var/www srcs_wordpress bash`
--- srcs/requirements/wordpress/run.sh
+++ srcs/requirements/wordpress/run.sh
@@ -19,13 +19,13 @@ then
 else
     echo create wp config...
 
-    wp core config --dbname="$DB_NAME" --dbhost="mariadb" --dbuser="$DB_ADMIN_USER" --dbpass="$DB_ADMIN_PASSWORD" --dbcollate="utf8_general_ci" --allow-root
+    wp core config --dbname="$DB_NAME" --dbhost="mariadb" --dbuser="$DB_ADMIN_NAME" --dbpass="$DB_ADMIN_PASSWORD" --dbcollate="utf8_general_ci" --allow-root
 
     # fix permissions
     chmod 644 wp-config.php
 
     # install wp core
-    wp core install --url="$DOMAIN" --title="$TITLE" --admin-user="$ADMIN_NAME" --admin-password="$ADMIN_PASSWORD" --skip-email --admin-email="yeet@yeet" --locale="en_GB" --allow-root
+    wp core install --url="$DOMAIN" --title="$TITLE" --admin_user="$ADMIN_NAME" --admin_password="$ADMIN_PASSWORD" --skip-email --admin_email="does_not_exist___@student.codam.nl" --locale="en_GB" --allow-root
 
     # TODO: user
 fi
